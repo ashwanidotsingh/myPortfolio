9aa4a41d1f6a0963b221410349896d75c10add7d
ashwani <ashwani.g.singh>
2021-04-30 14:27:54 +0530

    intial commit completed MyNavbar component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// components
+import MyNavbar from './components/my-navbar/mynavbar.component';
+
+const App=()=> {
+  return (
+    <div>
+      <MyNavbar/>
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;
